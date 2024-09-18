08edfd11b93f4a5908c275bb84dc3ae1a6bcf0dd
Abhishek Singh <abhisheks10202@gmail.com>
2024-09-18 23:21:39 +0530

    created Portfolio application

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIc5EjZmuy0nQ6E7iI7Zc_DkqJIbkBY_c",
+  authDomain: "portfolio-78a59.firebaseapp.com",
+  projectId: "portfolio-78a59",
+  storageBucket: "portfolio-78a59.appspot.com",
+  messagingSenderId: "500247331575",
+  appId: "1:500247331575:web:8955ed8f2bd958c09449f2",
+  measurementId: "G-P1FYELL06Z"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
